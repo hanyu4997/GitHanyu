e68720304ba91133856272678116533bf1da5ff1
Rem <rem@gmail.com>
2022-05-14 11:58:49 +0800

    Hanyu

---
+++ src/main/java/com/example/hanyugit/HanyuGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.hanyugit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanyuGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanyuGitApplication.class, args);
+	}
+
+}
